6d5eb1f2e0216e2d6d61a874d7a7b42b19871892
Eduardo <erfelipe@yahoo.com.br>
2020-10-04 19:55:45 -0300

    Primeira versao

---
+++ src/main.java
@@ -0,0 +1,13 @@
+import owlapi.tutorial.Tutorial;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		Tutorial tutorial = new Tutorial();
+		//tutorial.inicio();
+		tutorial.testeValidacao();
+	}
+
+}
+	
---
+++ src/owlapi/tutorial/Tutorial.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ */
+package owlapi.tutorial;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.expression.OWLEntityChecker;
+import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
+import org.semanticweb.owlapi.model.*;
+import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.CachingBidirectionalShortFormProvider;
+import org.semanticweb.owlapi.util.SimpleShortFormProvider;
+import org.semanticweb.owlapi.util.mansyntax.ManchesterOWLSyntaxParser;
+
+import java.io.File;
+
+/**
+ * Testes para conhecimento da OWL API e validacao de Axiomas
+ *
+ */
+class Provider extends CachingBidirectionalShortFormProvider {
+
+	private SimpleShortFormProvider provider = new SimpleShortFormProvider();
+
+	@Override
+	protected String generateShortForm(OWLEntity entity) {
+		return provider.getShortForm(entity);
+	}
+}
+
+public class Tutorial {
+	
+	public void inicio() {
+		
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		File arq = new File("/Users/eduardofelipe/Workspace/AxiomValidator/res/pizza.owl");
+		OWLOntology o;
+
+		try {
+			o = man.loadOntologyFromOntologyDocument(arq);
+			System.out.println(o);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		System.out.println(man.getOntologies().size());
+	}
+
+	public void testeAxioma() {
+		IRI iri = IRI.create("http://teste.qq.endereco");
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		try {
+			OWLOntology o = man.createOntology(iri);
+			OWLDataFactory df = o.getOWLOntologyManager().getOWLDataFactory();
+			OWLClass pessoa = df.getOWLClass(iri + "#Pessoa");
+			OWLDeclarationAxiom da = df.getOWLDeclarationAxiom(pessoa);
+			o.addAxiom(da);
+
+			OWLClass mulher = df.getOWLClass(iri + "#Mulher");
+			OWLSubClassOfAxiom m_sub_p = df.getOWLSubClassOfAxiom(mulher, pessoa);
+			o.addAxiom(m_sub_p);
+
+			System.out.println(o);
+		} catch (OWLOntologyCreationException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * https://github.com/owlcs/owlapi/issues/507
+	 * https://stackoverflow.com/questions/21005908/convert-string-in-manchester-syntax-to-owlaxiom-object-using-owlapi-3-in-java
+	 * https://github.com/owlcs/owlapi/wiki/DL-Queries-with-a-real-reasoner
+	 */
+	public void testeValidacao() {
+		OWLDataFactory df = OWLManager.getOWLDataFactory();
+
+		Provider shortFormProvider = new Provider();
+		OWLEntityChecker entityChecker = new ShortFormEntityChecker(shortFormProvider);
+		shortFormProvider.add(df.getOWLClass(IRI.create("http://example.org/Arm")));
+		shortFormProvider.add(df.getOWLClass(IRI.create("http://example.org/Finger")));
+		shortFormProvider.add(df.getOWLObjectProperty(IRI.create("http://example.org/hasPart")));
+
+		String input = "Arm subClassOf (hasPart some Finger)";
+		ManchesterOWLSyntaxParser parser = OWLManager.createManchesterParser();
+		parser.setOWLEntityChecker(entityChecker);
+		parser.setStringToParse(input);
+		OWLAxiom axiom = parser.parseAxiom();
+		System.out.println("CheckManchesterSyntax.main() " + axiom);
+	}
+}
