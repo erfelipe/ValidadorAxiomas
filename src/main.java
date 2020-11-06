be07eb2f13d1828d2116a5c11f67ec987bc57507
Eduardo <erfelipe@yahoo.com.br>
2020-11-06 18:01:46 -0300

    Versao estavel

--- src/main.java
+++ src/main.java
@@ -4,10 +4,24 @@ public class main {
 
 	public static void main(String[] args) {
 
-		Tutorial tutorial = new Tutorial();
-		//tutorial.inicio();
-//		tutorial.testeValidacao();
-		tutorial.testeAxioma();
+		String onto = "{\n" +
+				"    \"id\": \"https://onto4alleditor.com/pt/idDoProjeto/\",\n" +
+				"    \"formato\": \"OWL\",\n" +
+				"    \"classes\": [\"Pessoa\", \"Homem\", \"Mulher\"],\n" +
+				"    \"axiomas\": [\"Homem subClassOf (Pessoa)\", \"Mulher subClassOf (Pessoa)\"],\n" +
+				"    \"propriedades\": [\"hasPart\"]\n" +
+				"} ";
+		try {
+			Tutorial tutorial = new Tutorial();
+			//tutorial.inicio();
+			//tutorial.testeValidacao();
+			//tutorial.testeAxioma();
+			System.out.println(tutorial.formataOWL(onto));
+			tutorial.validaOWL(onto);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
--- src/owlapi/tutorial/Tutorial.java
+++ src/owlapi/tutorial/Tutorial.java
@@ -1,19 +1,23 @@
-/**
- * 
- */
+
 package owlapi.tutorial;
 
+import org.coode.owlapi.obo12.parser.OBO12DocumentFormat;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.expression.OWLEntityChecker;
 import org.semanticweb.owlapi.expression.ShortFormEntityChecker;
-import org.semanticweb.owlapi.formats.TurtleDocumentFormat;
+import org.semanticweb.owlapi.formats.*;
 import org.semanticweb.owlapi.model.*;
-import org.semanticweb.owlapi.util.BidirectionalShortFormProvider;
+
 import org.semanticweb.owlapi.util.CachingBidirectionalShortFormProvider;
 import org.semanticweb.owlapi.util.SimpleShortFormProvider;
 import org.semanticweb.owlapi.util.mansyntax.ManchesterOWLSyntaxParser;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 
 /**
  * Testes para conhecimento da OWL API e validacao de Axiomas
@@ -75,6 +79,13 @@ public class Tutorial {
 			OWLDocumentFormat formatoTurtle = new TurtleDocumentFormat();
 			try {
 				man.saveOntology(o, formatoTurtle, IRI.create(arqTurtle));
+
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				man.saveOntology(o, formatoTurtle, baos);
+
+				System.out.println("saida do stream");
+				System.out.println(baos.toString());
+
 			} catch (OWLOntologyStorageException e) {
 				e.printStackTrace();
 			}
@@ -100,11 +111,172 @@ public class Tutorial {
 		shortFormProvider.add(df.getOWLClass(IRI.create("http://example.org/Finger")));
 		shortFormProvider.add(df.getOWLObjectProperty(IRI.create("http://example.org/hasPart")));
 
-		String input = "Arm subClassOf (hasPart some Finger)";
+		String input = "Arma subClassOf (hasPart some Finger)";
 		ManchesterOWLSyntaxParser parser = OWLManager.createManchesterParser();
 		parser.setOWLEntityChecker(entityChecker);
 		parser.setStringToParse(input);
-		OWLAxiom axiom = parser.parseAxiom();
-		System.out.println("CheckManchesterSyntax.main() " + axiom);
+		try {
+			OWLAxiom axiom = parser.parseAxiom();
+			System.out.println("CheckManchesterSyntax.main() " + axiom.toString());
+		}
+		catch (Exception e) {
+			System.out.println("Informe o seguinte erro ao suporte: " + e.getMessage());
+		}
+	}
+
+	public String formataOWL(String ontologia) {
+		JSONObject owl = new JSONObject(ontologia);
+
+		String tipoFormato = owl.getString("formato");
+
+		OWLOntologyManager man = OWLManager.createOWLOntologyManager();
+		OWLDocumentFormat formato = getFormatoSaidaOntologia(tipoFormato);
+		try {
+			OWLOntology owlOntology = geraOWLdeString(ontologia);
+			if (owlOntology != null) {
+//				if (tipoFormato.equals("OWL")) {
+//					formato = new OWLXMLDocumentFormat();//man.getOntologyFormat(owlOntology);
+//				} else {
+//					formato = new TurtleDocumentFormat();
+//				}
+				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+				man.saveOntology(owlOntology, formato, baos);
+				return baos.toString();
+			} else {
+				return "Erro: null";
+			}
+		} catch (Exception e) {
+			return "Ocorreu o seguinte erro: " + e.getMessage();
+		}
+	}
+
+	public OWLOntology geraOWLdeString(String ontologia) throws OWLOntologyCreationException {
+		JSONObject owl = new JSONObject(ontologia);
+
+		String id = owl.getString("id");
+
+		IRI iri = IRI.create(id);
+		OWLOntologyManager owlManager = OWLManager.createOWLOntologyManager();
+		OWLOntology owlOntology;
+
+		owlOntology = owlManager.createOntology(iri);
+		OWLDataFactory dataFactory = owlOntology.getOWLOntologyManager().getOWLDataFactory();
+
+		Provider shortFormProvider = new Provider();
+		OWLEntityChecker entityChecker = new ShortFormEntityChecker(shortFormProvider);
+		/**
+		 * Trabalha se as classes
+		 */
+		JSONArray classes = new JSONArray();
+		classes = owl.getJSONArray("classes");
+
+		for (int i = 0; i < classes.length(); i++) {
+			shortFormProvider.add(dataFactory.getOWLClass(iri.toString() + "#" + classes.get(i)));
+		}
+
+		/**
+		 * Trabalha se os axiomas declarativos
+		 */
+		JSONArray axiomas = new JSONArray();
+		axiomas = owl.getJSONArray("axiomas");
+		ManchesterOWLSyntaxParser parser = OWLManager.createManchesterParser();
+		parser.setOWLEntityChecker(entityChecker);
+
+		try {
+			for (int i = 0; i < axiomas.length(); i++) {
+				parser.setStringToParse(axiomas.getString(i));
+				owlOntology.addAxiom(parser.parseAxiom());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		return owlOntology;
+	}
+
+	public String validaOWL(String ontologia) {
+		OWLDataFactory df = OWLManager.getOWLDataFactory();
+
+		Provider shortFormProvider = new Provider();
+		OWLEntityChecker entityChecker = new ShortFormEntityChecker(shortFormProvider);
+
+		//JSONObject owl2 = this.preparaElementos();
+		//System.out.println(owl2);
+		JSONObject owl = new JSONObject(ontologia);
+
+		/**
+		 * Trabalha se as classes
+		 */
+		JSONArray classes = new JSONArray();
+		classes = owl.getJSONArray("classes");
+
+		for (int i = 0; i < classes.length(); i++) {
+			shortFormProvider.add(df.getOWLClass(IRI.create("https://onto4alleditor.com/pt/idDoProjeto/" + classes.get(i))));
+		}
+
+		/**
+		 * Trabalha se os axiomas
+		 */
+		JSONArray axiomas = new JSONArray();
+		axiomas = owl.getJSONArray("axiomas");
+		ManchesterOWLSyntaxParser parser = OWLManager.createManchesterParser();
+		parser.setOWLEntityChecker(entityChecker);
+
+		for (int i = 0; i < axiomas.length(); i++) {
+			parser.setStringToParse(axiomas.getString(i));
+		}
+
+		try {
+			OWLAxiom axiom = parser.parseAxiom();
+			return "Axioma válido " + axiom.toString();
+		} catch (Exception e) {
+			return "Axioma inválido: " + e.toString();
+		}
+	}
+
+	public OWLDocumentFormat getFormatoSaidaOntologia(String formato) {
+		OWLDocumentFormat documentFormat = null;
+
+		if (formato.equals("OWL")) {
+			documentFormat = new OWLXMLDocumentFormat();
+		} else if (formato.equals("TURTLE")) {
+			documentFormat = new TurtleDocumentFormat();
+		} else if (formato.equals("SINTAXEDL")) {
+			documentFormat = new DLSyntaxDocumentFormat();
+		} else if (formato.equals("SINTAXEDLHTML")) {
+			documentFormat = new DLSyntaxHTMLDocumentFormat();
+		} else if (formato.equals("SINTAXEFUNCIONAL")) {
+			documentFormat = new FunctionalSyntaxDocumentFormat();
+		} else if (formato.equals("KRSS")) {
+			documentFormat = new KRSS2DocumentFormat();
+		} else if (formato.equals("DOCUMENTOLATEX")) {
+			documentFormat = new LatexDocumentFormat();
+		} else if (formato.equals("N3")) {
+			documentFormat = new N3DocumentFormat();
+		} else if (formato.equals("SINTAXEMANCHERTER")) {
+			documentFormat = new ManchesterSyntaxDocumentFormat();
+		} else if (formato.equals("NQUAD")) {
+			documentFormat = new NQuadsDocumentFormat();
+		} else if (formato.equals("NTRIPLA")) {
+			documentFormat = new NTriplesDocumentFormat();
+		} else if (formato.equals("OBO")) {
+			documentFormat = new OBODocumentFormat();
+		} else if (formato.equals("RDFJSON")) {
+			documentFormat = new RDFJsonDocumentFormat();
+		} else if (formato.equals("RDFJSONLD")) {
+			documentFormat = new RDFJsonLDDocumentFormat();
+		} else if (formato.equals("RDFXML")) {
+			documentFormat = new RDFXMLDocumentFormat();
+		} else if (formato.equals("RIOTURTLE")) {
+			documentFormat = new RioTurtleDocumentFormat();
+		} else if (formato.equals("RIORDFXML")) {
+			documentFormat = new RioRDFXMLDocumentFormat();
+		} else if (formato.equals("TRIG")) {
+			documentFormat = new TrigDocumentFormat();
+		} else if (formato.equals("TRIX")) {
+			documentFormat = new TrixDocumentFormat();
+		}
+		return documentFormat;
 	}
 }
+
